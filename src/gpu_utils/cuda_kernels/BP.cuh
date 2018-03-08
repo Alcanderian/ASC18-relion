15b94c13f64f5fe52ec894018df8906a0fe7b591
alcanderian <alcanderian@gmail.com>
2018-03-08 10:47:12 +0800

    fix bug of autorefine_mpi with cuda

--- src/gpu_utils/cuda_kernels/BP.cuh
+++ src/gpu_utils/cuda_kernels/BP.cuh
@@ -46,14 +46,18 @@ __global__ void cuda_kernel_backproject2D(
 
 	XFLOAT minvsigma2, ctf, img_real, img_imag, Fweight, real, imag, weight;
 
-	if (tid == 0)
-		s_eulers[0] = g_eulers[img*9+0] * padding_factor;
-	else if (tid == 1)
-		s_eulers[1] = g_eulers[img*9+1] * padding_factor;
-	else if (tid == 2)
-		s_eulers[2] = g_eulers[img*9+3] * padding_factor;
-	else if (tid == 3)
-		s_eulers[3] = g_eulers[img*9+4] * padding_factor;
+	// opt by ljx 2018.3.7
+	//if (tid == 0)
+	//	s_eulers[0] = g_eulers[img*9+0] * padding_factor;
+	//else if (tid == 1)
+	//	s_eulers[1] = g_eulers[img*9+1] * padding_factor;
+	//else if (tid == 2)
+	//	s_eulers[2] = g_eulers[img*9+3] * padding_factor;
+	//else if (tid == 3)
+	//	s_eulers[3] = g_eulers[img*9+4] * padding_factor;
+	if (tid <= 4)
+		s_eulers[tid] = g_eulers[img*9+tid] * padding_factor;
+	// end opt
 
 	__syncthreads();
 
